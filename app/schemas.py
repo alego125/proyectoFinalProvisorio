47877620220961769651bb875147311d7393c223
Ale Gomez <alejandro.gomez969@gmail.com>
2022-11-28 18:25:13 -0300

    primer commit agregado proyecto completo

---
+++ app/schemas.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class order_reviews(BaseModel):
+    pass
+
+
+class oreder_paiments(BaseModel):
+    pass
+
+
+class orders(BaseModel):
+    pass
+
+
+class order_customer(BaseModel):
+    pass
+
+
+class products(BaseModel):
+    pass
+
+
+class order_items(BaseModel):
+    pass
+
+
+class sellers(BaseModel):
+    pass
+
+
+class geolocation(BaseModel):
+    pass
